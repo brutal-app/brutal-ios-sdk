56be96dbf1ffdaf45ff1855fd5e40990c82a1491
Tiago Martinho <t.martinho@icloud.com>
2019-01-14 12:47:48 +0100

    Generate Project using swiftplate

---
+++ Sources/Brutal.swift
@@ -0,0 +1,9 @@
+//
+//  Brutal.swift
+//  Brutal
+//
+//  Created by Tiago Martinho on 14/01/2019.
+//  Copyright © 2019 Brutal. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/BrutalTests/BrutalTests.swift
@@ -0,0 +1,23 @@
+//
+//  BrutalTests.swift
+//  Brutal
+//
+//  Created by Tiago Martinho on 14/01/2019.
+//  Copyright © 2019 Brutal. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import Brutal
+
+class BrutalTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(Brutal().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
